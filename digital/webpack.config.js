c7aa14b09baf36223c9b0b74c8ea15a927db4b2b
Daria <allacia.gilbert@gmail.com>
2019-03-05 19:05:15 +0200

    add section with quote, hover for button

--- digital/webpack.config.js
+++ digital/webpack.config.js
@@ -2,25 +2,29 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-	  rules: [
-		{
-		  test: /\.scss$/,
-		  use: ExtractTextPlugin.extract({
-			fallback: 'style-loader',
-			use: ['css-loader', 'sass-loader']
-		  })
-		},
-		{
-		    test: /\.(png|jpg|jpeg|svg|gif)$/,
-			loader: "file-loader?name=src/img/[name].[ext]"
-		}
-      ]
+	entry: './src/app.js',
+	output: {
+	path: path.resolve(__dirname, 'dist'),
+	filename: 'bundle.js'
+	},
+	module: {
+		  rules: [
+				{
+				  test: /\.scss$/,
+				  use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				  })
+				},
+				{
+					test: /\.(png|jpg|jpeg|svg|gif)$/,
+					loader: "file-loader?name=src/img/[name].[ext]"
+				},
+			    {
+			    	test: /\.(eot|svg|ttf|woff|woff2)$/,
+					loader: 'file-loader?name=src/fonts/[name].[ext]'
+			    }
+		  ]
 	},
 	plugins: [
 		new ExtractTextPlugin('style.css')
